77134d5ae97e7b05f329a3521cda47ec43e172ce
mcbridn <photovor@gmail.com>
2017-06-27 09:58:59 -0400

    Generally cleaned up JS Code. Added additional inline comments

--- get_last_video.php
+++ get_last_video.php
@@ -1,4 +1,9 @@
 <?php
+    /*
+     * A quick script that is called via a GET request that will echo out the last video recorded
+     * by looking at the timestamps in the directory
+     */
+
     $user = $_GET['username'];
     $files = glob("../videos/$user/*.*");
     $files = array_combine($files, array_map('filectime', $files));
